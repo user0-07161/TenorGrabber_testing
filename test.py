7189d2fb15badaad84173cb4e3786cf4234005a8
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-29 10:23:43 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,2 @@
+from TenorGrabber import tenorgrabber
+tenorgrabber.getgiflink("https://tenor.com/view/hamburger-day-may-28-international-hamburger-day-happy-hamburger-day-it's-hamburger-day-gif-16923804985416609027")
