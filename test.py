d93ba6607340b09f13587271956b1aec1947a90c
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-29 10:47:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 from TenorGrabber import tenorgrabber
-tenorgrabber.getgiflink("https://tenor.com/view/hamburger-day-may-28-international-hamburger-day-happy-hamburger-day-it's-hamburger-day-gif-16923804985416609027")
+print(tenorgrabber.getgiflink("https://tenor.com/view/hamburger-day-may-28-international-hamburger-day-happy-hamburger-day-it's-hamburger-day-gif-16923804985416609027"))
